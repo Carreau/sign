03ff6a081fba0befa085f9c0ad74d83c0ea5e040
Matthias Bussonnier <bussonniermatthias@gmail.com>
2016-03-09 10:33:53 -0800

    Initial commit of Sign

--- sign/__init__.py
+++ sign/__init__.py
@@ -6,4 +6,13 @@ a simple package
 
 __version__ = '0.0.1'
 
-        
+import inspect
+
+
+def inherit(target):
+
+    def decorate(function):
+        function.__signature__ = inspect.signature(target)
+        return function
+
+    return decorate
